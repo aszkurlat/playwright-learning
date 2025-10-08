d40804ed3de89d04d6817db508a38e857063379e
Agnieszka Szkurłat <szkurlatagnieszka@gmail.com>
2025-10-08 18:46:52 +0200

    refactor: update DemoBank login tests with correct credentials

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -3,22 +3,17 @@ import { test, expect } from '@playwright/test';
 test.describe('User login to Demobank', () => {
   test('successful login with correct credentials', async ({ page }) => {
     await page.goto('https://demo-bank.vercel.app/');
-    await page.getByTestId('login-input').click();
-    await page.getByTestId('login-input').fill('testExmp');
-    await page.getByTestId('password-input').click();
-    await page.getByTestId('password-input').fill('10987654');
+    await page.getByTestId('login-input').fill('UserTest');
+    await page.getByTestId('password-input').fill('Qwerty12');
     await page.getByTestId('login-button').click();
-    await page.getByTestId('user-name').click();
 
     await expect(page.getByTestId('user-name')).toHaveText('Jan Demobankowy');
   });
 
   test('Unsuccessful login with too short username', async ({ page }) => {
     await page.goto('https://demo-bank.vercel.app/');
-    await page.getByTestId('login-input').click();
-    await page.getByTestId('login-input').fill('tester');
+    await page.getByTestId('login-input').fill('User1');
     await page.getByTestId('password-input').click();
-    await page.getByTestId('error-login-id').click();
 
     await expect(page.getByTestId('error-login-id')).toHaveText(
       'identyfikator ma min. 8 znaków'
@@ -27,9 +22,7 @@ test.describe('User login to Demobank', () => {
 
   test('Unsuccessful login with too short password', async ({ page }) => {
     await page.goto('https://demo-bank.vercel.app/');
-    await page.getByTestId('login-input').click();
-    await page.getByTestId('login-input').fill('testerEx');
-    await page.getByTestId('password-input').click();
+    await page.getByTestId('login-input').fill('User1');
     await page.getByTestId('password-input').fill('Qwe');
     await page.getByTestId('password-input').blur();
 
