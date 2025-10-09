e7c2c2fc9a56cfcd512b90a8a7e9cdf74330d138
Agnieszka Szkurłat <szkurlatagnieszka@gmail.com>
2025-10-09 13:40:02 +0200

    refactor: apply Arrange–Act–Assert (AAA) pattern to test structure

--- tests/dashboard.spec.ts
+++ tests/dashboard.spec.ts
@@ -2,20 +2,32 @@ import { test, expect } from '@playwright/test';
 
 test.describe('Dashboard tests', () => {
     test('Quick payment with correct data', async ({ page }) => {
-        await page.goto('https://demo-bank.vercel.app/');
-        await page.getByTestId('login-input').fill('User_222');
-        await page.getByTestId('password-input').fill('password');
+        // Arrange
+        const url = 'https://demo-bank.vercel.app/';
+        const userId = 'UserTest';
+        const userPassword = 'Qwerty12';
+
+        const receiverId = '2';
+        const transferAmount = '150';
+        const transferTitle = 'Nike Shoes';
+        const expectedTransferReceiver = 'Chuck Demobankowy';
+
+        // Act
+        await page.goto(url);
+        await page.getByTestId('login-input').fill(userId);
+        await page.getByTestId('password-input').fill(userPassword);
         await page.getByTestId('login-button').click();
 
-        await page.locator('#widget_1_transfer_receiver').selectOption('2');
-        await page.locator('#widget_1_transfer_amount').fill('150');
-        await page.locator('#widget_1_transfer_title').fill('pizza');
+        await page.locator('#widget_1_transfer_receiver').selectOption(receiverId);
+        await page.locator('#widget_1_transfer_amount').fill(transferAmount);
+        await page.locator('#widget_1_transfer_title').fill(transferTitle);
 
         await page.getByRole('button', { name: 'wykonaj' }).click();
         await page.getByTestId('close-button').click();
 
+        // Assert
         await expect(page.locator('#show_messages')).toHaveText(
-            'Przelew wykonany! Chuck Demobankowy - 150,00PLN - pizza'
+            `Przelew wykonany! ${expectedTransferReceiver} - 150,00PLN - Nike Shoes`
         );
     });
 });
--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,33 +1,56 @@
 import { test, expect } from '@playwright/test';
 
 test.describe('User login to Demobank', () => {
-  test('successful login with correct credentials', async ({ page }) => {
-    await page.goto('https://demo-bank.vercel.app/');
-    await page.getByTestId('login-input').fill('UserTest');
-    await page.getByTestId('password-input').fill('Qwerty12');
+
+  test('Successful login with correct credentials', async ({ page }) => {
+    // Arrange
+    const url = 'https://demo-bank.vercel.app/';
+    const userId = 'UserTest';
+    const userPassword = 'Qwerty12';
+    const expectedUserName = 'Jan Demobankowy';
+
+    // Act
+    await page.goto(url);
+    await page.getByTestId('login-input').fill(userId);
+    await page.getByTestId('password-input').fill(userPassword);
     await page.getByTestId('login-button').click();
 
-    await expect(page.getByTestId('user-name')).toHaveText('Jan Demobankowy');
+    // Assert
+    await expect(page.getByTestId('user-name')).toHaveText(expectedUserName);
   });
 
   test('Unsuccessful login with too short username', async ({ page }) => {
-    await page.goto('https://demo-bank.vercel.app/');
-    await page.getByTestId('login-input').fill('User1');
+    // Arrange
+    const url = 'https://demo-bank.vercel.app/';
+    const userId = 'User';
+    const expectedErrorMessage = 'identyfikator ma min. 8 znaków';
+
+    // Act
+    await page.goto(url);
+    await page.getByTestId('login-input').fill(userId);
     await page.getByTestId('password-input').click();
 
+
+    //Assert
     await expect(page.getByTestId('error-login-id')).toHaveText(
-      'identyfikator ma min. 8 znaków'
+      expectedErrorMessage
     );
   });
 
   test('Unsuccessful login with too short password', async ({ page }) => {
-    await page.goto('https://demo-bank.vercel.app/');
-    await page.getByTestId('login-input').fill('User1');
-    await page.getByTestId('password-input').fill('Qwe');
+    // Arrange
+    const url = 'https://demo-bank.vercel.app/';
+    const userId = 'User1';
+    const userPassword = 'Qwe';
+    const expectedErrorMessage = 'hasło ma min. 8 znaków';
+
+    // Act
+    await page.goto(url);
+    await page.getByTestId('login-input').fill(userId);
+    await page.getByTestId('password-input').fill(userPassword);
     await page.getByTestId('password-input').blur();
 
-    await expect(page.getByTestId('error-login-password')).toHaveText(
-      'hasło ma min. 8 znaków'
-    );
+    // Assert
+    await expect(page.getByTestId('error-login-password')).toHaveText(expectedErrorMessage);
   });
 });
