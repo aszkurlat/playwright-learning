a48128357929577c4f6d27353b05ec799f950a4a
Agnieszka Szkurłat <szkurlatagnieszka@gmail.com>
2025-10-10 11:00:27 +0200

    Add global beforeEach hook and set baseURL for tests

--- tests/dashboard.spec.ts
+++ tests/dashboard.spec.ts
@@ -1,23 +1,26 @@
 import { test, expect } from '@playwright/test';
 
 test.describe('Dashboard tests', () => {
+    const userId = 'UserTest';
+    const userPassword = 'Qwerty12';
+
+    test.beforeEach(async ({ page }) => {
+        await page.goto("/");
+        await page.getByTestId('login-input').fill(userId);
+        await page.getByTestId('password-input').fill(userPassword);
+        await page.getByTestId('login-button').click();
+
+        await page.waitForURL('https://demo-bank.vercel.app/pulpit.html');
+    })
+
     test('Quick payment with correct data', async ({ page }) => {
         // Arrange
-        const url = 'https://demo-bank.vercel.app/';
-        const userId = 'UserTest';
-        const userPassword = 'Qwerty12';
-
         const receiverId = '2';
         const transferAmount = '150';
         const transferTitle = 'Nike Shoes';
         const expectedTransferReceiver = 'Chuck Demobankowy';
 
         // Act
-        await page.goto(url);
-        await page.getByTestId('login-input').fill(userId);
-        await page.getByTestId('password-input').fill(userPassword);
-        await page.getByTestId('login-button').click();
-
         await page.locator('#widget_1_transfer_receiver').selectOption(receiverId);
         await page.locator('#widget_1_transfer_amount').fill(transferAmount);
         await page.locator('#widget_1_transfer_title').fill(transferTitle);
--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,16 +1,16 @@
 import { test, expect } from '@playwright/test';
 
 test.describe('User login to Demobank', () => {
-
+  test.beforeEach(async ({ page }) => {
+    await page.goto("/");
+  });
   test('Successful login with correct credentials', async ({ page }) => {
     // Arrange
-    const url = 'https://demo-bank.vercel.app/';
     const userId = 'UserTest';
     const userPassword = 'Qwerty12';
     const expectedUserName = 'Jan Demobankowy';
 
     // Act
-    await page.goto(url);
     await page.getByTestId('login-input').fill(userId);
     await page.getByTestId('password-input').fill(userPassword);
     await page.getByTestId('login-button').click();
@@ -21,12 +21,10 @@ test.describe('User login to Demobank', () => {
 
   test('Unsuccessful login with too short username', async ({ page }) => {
     // Arrange
-    const url = 'https://demo-bank.vercel.app/';
     const userId = 'User';
     const expectedErrorMessage = 'identyfikator ma min. 8 znaków';
 
     // Act
-    await page.goto(url);
     await page.getByTestId('login-input').fill(userId);
     await page.getByTestId('password-input').click();
 
@@ -39,13 +37,12 @@ test.describe('User login to Demobank', () => {
 
   test('Unsuccessful login with too short password', async ({ page }) => {
     // Arrange
-    const url = 'https://demo-bank.vercel.app/';
     const userId = 'User1';
     const userPassword = 'Qwe';
     const expectedErrorMessage = 'hasło ma min. 8 znaków';
 
     // Act
-    await page.goto(url);
+
     await page.getByTestId('login-input').fill(userId);
     await page.getByTestId('password-input').fill(userPassword);
     await page.getByTestId('password-input').blur();
