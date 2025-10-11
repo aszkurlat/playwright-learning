4071604ef244a7fdd29b35851161a8288567704e
Agnieszka Szkurłat <szkurlatagnieszka@gmail.com>
2025-10-11 20:02:20 +0200

    test: add mobile top-up and balance verification tests in dashboard.spec.ts

---
+++ tests/payment.spec.ts
@@ -0,0 +1,34 @@
+import { test, expect } from '@playwright/test';
+import { userLogin } from '../test-data/login.data';
+import { LoginPage } from '../pages/login.page';
+
+
+test.describe('Payment tests', () => {
+    test.beforeEach(async ({ page }) => {
+        await page.goto('/');
+        const loginPage = new LoginPage(page);
+        await loginPage.loginInput.fill(userLogin.userId);
+        await loginPage.passwordInput.fill(userLogin.userPassword);
+        await loginPage.loginButton.click();
+
+        await page.getByRole('link', { name: 'płatności' }).click();
+    });
+
+    test('Simple payment', async ({ page }) => {
+        // Arrange
+        const transferReceiver = 'Jan Nowak';
+        const transferAccount = '12 3456 7890 1234 5678 9012 34568';
+        const transferAmount = '222';
+        const expectedMessage = `Przelew wykonany! ${transferAmount},00PLN dla Jan Nowak`;
+
+        // Act
+        await page.getByTestId('transfer_receiver').fill(transferReceiver);
+        await page.getByTestId('form_account_to').fill(transferAccount);
+        await page.getByTestId('form_amount').fill(transferAmount);
+        await page.getByRole('button', { name: 'wykonaj przelew' }).click();
+        await page.getByTestId('close-button').click();
+
+        // Assert
+        await expect(page.locator('#show_messages')).toHaveText(expectedMessage);
+    });
+});
