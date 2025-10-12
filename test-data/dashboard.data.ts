3ab6b116b26d160e5871da20fd7a7ecc187baca0
Agnieszka Szkurłat <szkurlatagnieszka@gmail.com>
2025-10-12 17:55:01 +0200

    test: add test data files for dashboard and payment pages

---
+++ test-data/dashboard.data.ts
@@ -0,0 +1,8 @@
+export const dashboardData = {
+    receiverId: '2',
+    transferAmount: '150',
+    transferTitle: 'Nike Shoes',
+    expectedTransferReceiver: 'Chuck Demobankowy',
+    topUpReceiver: '500 xxx xxx',
+    topUpAmount: '50'
+}
